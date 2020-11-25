f93b3cfcd86dfb97679b006744e06c7c19128634
aloksingh1408 <73990522+aloksingh1408@users.noreply.github.com>
2020-11-25 11:30:14 +0530

    Add files via upload

---
+++ pratical5.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int a,b,c;
+	cin>>a >>b >>c;
+	if(a>b && a>c){
+		cout<<"Greatest number is " <<a <<"\n";
+		}else if(b>a && b>c){
+			cout<<"Greatest number is " <<b <<"\n";
+			}else{
+				cout<<"Greatest number is " <<c <<"\n";
+				} 
+	}
