8c0b4592e0dfb8f1263379d75a1bd865c8b6a7df
aloksingh1408 <73990522+aloksingh1408@users.noreply.github.com>
2020-11-25 11:32:27 +0530

    Add files via upload

---
+++ pratical3.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int a,b;
+	cin>>a >>b;
+	cout<<"Before Swapping" <<"\n";
+	cout<<"Value of a: " <<a <<"\n";
+	cout<<"Value of b: " <<b <<"\n";
+	swap(a, b);
+	cout<<"After Swapping" <<"\n";
+	cout<<"Value of a: " <<a <<"\n";
+	cout<<"Value of b: " <<b <<"\n";
+}
