0e0cd811ff9f172f1e555f65e5ccf16748b3dcd5
aloksingh1408 <73990522+aloksingh1408@users.noreply.github.com>
2020-11-25 11:32:50 +0530

    Add files via upload

---
+++ pratical4.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int a;
+	cin>>a;
+	if(a%2==0){
+		cout<<"The number is even" <<"\n";
+		}else{
+			cout<<"The number is odd" <<"\n";
+			}
+	}
