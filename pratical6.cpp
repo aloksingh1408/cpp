d7b71aa0183a2200b3f35d2690a1e5437849c2ce
aloksingh1408 <73990522+aloksingh1408@users.noreply.github.com>
2020-11-25 11:30:46 +0530

    Add files via upload

---
+++ pratical6.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Person{
+	public:
+	string name;
+	int age;
+	};
+
+int main(){
+	Person obj;
+	obj.name="Niraj gupta";
+	obj.age=20;
+	cout<<obj.name <<"\n";
+	cout<<obj.age <<"\n";
+}
